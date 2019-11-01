e66f6aad3972f352848d33d5b33d8a3f8f6584d3
Mona B <web25Lucius@gmail.com>
2019-11-01 16:56:56 -0600

    done with all three, returning out of functions

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,37 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+axios.get("https://lambda-times-backend.herokuapp.com/articles").then (response=>{
+    // console.log(response.data.articles); 
+    for(articles in response.data) {
+        let newCard = document.createElement('div'); 
+        newCard.classList.add('.card'); 
+ 
+        let newHeadline = document.createElement('div'); 
+        newHeadline.classList.add('.headline'); 
+        newCard.appendChild(newHeadline); 
+
+        let newAuthor = document.createElement('div'); 
+        newAuthor.classList.add('.author'); 
+        newCard.appendChild(newAuthor); 
+        
+        let newImgContainer = document.createElement('div'); 
+        newImgContainer.classList.add('.img-container'); 
+        newCard.appendChild(newImgContainer); 
+
+        let newImg = document.createElement('img'); 
+        newImg.setAttribute=('src', 'articles.authorPhoto');
+        newImgContainer.appendChild(newImg); 
+
+        let newAuthorName = document.createElement('span'); 
+        newAuthorName.setAttribute=('articles.authorName');
+        
+        console.log(newAuthorName); 
+        return newCard; 
+    
+    }
+         document.querySelector('.card-container').appendChild(newCard); 
+})   
+
+
+
